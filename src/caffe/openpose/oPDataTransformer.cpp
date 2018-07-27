b3c7e5a6f0baefa02e15b84c48c4d4e6ff809337
gineshidalgo99 <gineshidalgo99@gmail.com>
2018-07-27 23:58:32 +0200

    Visaulize on

--- src/caffe/openpose/oPDataTransformer.cpp
+++ src/caffe/openpose/oPDataTransformer.cpp
@@ -1052,8 +1052,8 @@ void maskFeet(cv::Mat& maskMiss, const std::vector<float>& isVisible, const std:
             const auto ankle = ratioStride * points.at(ankleIndex);
             const auto distance = (int)std::round(ratio*getNorm(knee, ankle));
             const cv::Point momentum = (ankle-knee)*0.15f;
-            cv::Rect roi{(int)std::round(ankle.x /*+ momentum.x*/)-distance,
-                         (int)std::round(ankle.y /*+ momentum.y*/)-distance,
+            cv::Rect roi{(int)std::round(ankle.x + momentum.x)-distance,
+                         (int)std::round(ankle.y + momentum.y)-distance,
                          2*distance, 2*distance};
             // Apply ROI
             keepRoiInside(roi, maskMiss.size());
